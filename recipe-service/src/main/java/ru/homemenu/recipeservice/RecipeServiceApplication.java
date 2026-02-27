6d50b1fa7a3f40df073e27dee3c339f3d9ef68c5
Zaytsev Andrey <zaytsev.andrey.al@yandex.ru>
2026-02-28 00:03:07 +0300

    Initial commit

---
+++ recipe-service/src/main/java/ru/homemenu/recipeservice/RecipeServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.homemenu.recipeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(RecipeServiceApplication.class, args);
+    }
+}
