c2b0ad42544c15ed4f5aa6e60ddc5f8a5727f5bd
Zaytsev Andrey <zaytsev.andrey.al@yandex.ru>
2026-03-08 14:10:34 +0300

    Add NoResourceFoundException handler

--- recipe-service/src/main/java/ru/homemenu/recipeservice/log/StructuredLogEvent.java
+++ recipe-service/src/main/java/ru/homemenu/recipeservice/log/StructuredLogEvent.java
@@ -5,9 +5,10 @@ public final class StructuredLogEvent {
     private StructuredLogEvent() {
     }
 
-    public static final String REQUEST_VALIDATION_FAILED = "request_validation_failed";
     public static final String CONSTRAINT_VIOLATION = "constraint_violation";
     public static final String JSON_PARSE_ERROR = "json_parse_error";
+    public static final String REQUEST_VALIDATION_FAILED = "request_validation_failed";
+    public static final String RESOURCE_NOT_FOUND = "resource_not_found";
     public static final String UNEXPECTED_ERROR = "unexpected_error";
 
 }
